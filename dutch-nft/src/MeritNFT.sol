98cd7248a72b6966b2d78ca003d3a27a19300c35
sherlock-admin <dev@sherlock.xyz>
2023-07-10 09:51:17 +0000

    Copy dutch-nft @ 822c3c546227ded78e791cf5b0d2addc59dd0214

---
+++ dutch-nft/src/MeritNFT.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract MeritNFT is ERC721Enumerable, AccessControlEnumerable {
+    using Strings for uint256;
+
+    // Emitted when a non minter calls minter only function
+    error OnlyMinterError();
+    // Emitted when a non uri setter calls uri setter only function
+    error OnlyURISetter();
+
+    // Role for minters
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    // Role for setting the baseURI
+    bytes32 public constant URI_SETTER = keccak256("URI_SETTER");
+    // Prefix for the tokenURI. tokenURI = baseTokenURI + tokenId + .json
+    string internal baseTokenURI_;
+
+    // Modifier for minter only functions
+    modifier onlyMinter {
+        if(!hasRole(MINTER_ROLE, msg.sender)) {
+            revert OnlyMinterError();
+        }
+        _;
+    }
+
+    // Modifier for uri setter only functions
+    modifier onlyUriSetter {
+        if(!hasRole(URI_SETTER, msg.sender)) {
+            revert OnlyURISetter();
+        }
+        _;
+    }
+
+    /// @notice Constructor
+    /// @param _name The name of the NFT
+    /// @param _symbol Symbol aka ticker
+    /// @param _baseTokenURI Prepends the tokenId for the tokenURI
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseTokenURI,
+        address _uriSetter,
+        address _minter
+    ) ERC721(_name, _symbol) {
+        baseTokenURI_ = _baseTokenURI;
+        _setupRole(URI_SETTER, _uriSetter);
+        _setupRole(MINTER_ROLE, _minter);
+    }
+
+
+    /// @notice Mints an NFT. Can only be called by an address with the minter role and tokenId must be unique
+    /// @param _tokenId Id of the token
+    /// @param _receiver Address receiving the NFT
+    function mint(uint256 _tokenId, address _receiver) external onlyMinter {
+        _mint(_receiver, _tokenId);
+    }
+
+    /// @notice Sets the base token URI. Can only be called by an address with the default admin role
+    /// @param _newBaseURI New baseURI
+    function setBaseURI(string memory _newBaseURI) external onlyUriSetter {
+        baseTokenURI_ = _newBaseURI;
+    }
+
+    /// @notice Returns the baseURI
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI_;
+    }
+
+    /// @notice returns the baseURI
+    /// @return The tokenURI
+    function baseURI() external view returns (string memory) {
+        return _baseURI();
+    }
+
+    /// @notice Signals support for a given interface
+    /// @param interfaceId 4bytes signature of the interface
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerable, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI_ = _baseURI();
+        return bytes(baseURI_).length > 0 ? string(abi.encodePacked(baseURI_, tokenId.toString(), ".json")) : "";
+    }
+}
